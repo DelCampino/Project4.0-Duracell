54c14ec6d5bf3ad292b5e2e409299a9ea97b5c45
Jeroen246 <r0695294@student.thomasmore.be>
2020-01-28 09:10:19 +0100

    navbar aanpassing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { Stomp } from "stomp.js";
 import { BehaviorSubject } from 'rxjs';
 import { RabbitmqService } from './services/rabbitmq.service';
+import { ThemeService } from './services/theme.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
+
 export class AppComponent implements OnInit {
   ws = null;
   client = null;
+
 
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
-    private rabbitmqservice: RabbitmqService
+    private rabbitmqservice: RabbitmqService,
+    private themeSwitcher: ThemeService
   ) 
   {
+
     this.initializeApp();
+    
     this.rabbitmqservice.group.subscribe(e=> {
       console.log("Changing queue to:" + e);
       //if (this.client != null) {
       //  this.client.disconnect();
       //}
 
-    
-
       this.changeQueue(e);
-
     
     });
   }
@@ -52,14 +54,14 @@ export class AppComponent implements OnInit {
 
 
   changeQueue(toQueue) {
-    this.ws = new WebSocket('ws://192.168.1.2:15674/ws'); // SERVER
-    //this.ws = new WebSocket('ws://localhost:15674/ws'); // LOCAL
+    //this.ws = new WebSocket('ws://192.168.1.2:15674/ws'); // SERVER
+    this.ws = new WebSocket('ws://localhost:15674/ws'); // LOCAL
     this.client = Stomp.over(this.ws);
 
     var queue = '/queue/' + toQueue
     var bind = this;
     var on_connect = function() {
-      //alert("connected to new queue: " + toQueue)
+      alert("connected")
       bind.client.subscribe(queue, function(message) {
         console.log("Message received: " + message);
         bind.updateMessages(message);
@@ -70,13 +72,26 @@ export class AppComponent implements OnInit {
     };
 
     
-    this.client.connect('team4', 'team4', on_connect, on_error, 'team4vhost'); // SERVER
-    //this.client.connect('guest', 'guest', on_connect, on_error, '/'); // LOCAL
+    //this.client.connect('team4', 'team4', on_connect, on_error, 'team4vhost'); // SERVER
+    this.client.connect('guest', 'guest', on_connect, on_error, '/'); // LOCAL
   }
 
   updateMessages(message) {
     this.rabbitmqservice.messages.next([...this.rabbitmqservice.messages.value, message]);
     console.log(this.rabbitmqservice.messages.value);
   }
+
+  ThemeSwitcher() {
+    // 0 = day mode
+    // 1 = night mode
+    if (this.themeSwitcher.currentTheme === 0) {
+      this.themeSwitcher.setTheme('night');
+      this.themeSwitcher.currentTheme = 1;
+    } else {
+      this.themeSwitcher.setTheme('day');
+      this.themeSwitcher.currentTheme = 0;
+    }
+  }
+
 
 }
