9888297198ae5d6f061b1517d9b1af8a3d165609
Jeroen246 <r0695294@student.thomasmore.be>
2020-01-29 09:22:00 +0100

    bug fix

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-import { Component, Output } from '@angular/core';
-import { EventEmitter } from 'protractor';
-=======
 import { Component } from '@angular/core';
 import { RabbitmqService } from '../services/rabbitmq.service';
->>>>>>> e93162ad8424bab257189061df5d9434f67bffaf
 
 @Component({
   selector: 'app-tabs',
