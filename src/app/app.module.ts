42b37d091fe0549b9c3ecd81c886e12efa88f462
Sacha Kempeneer <sachakempeneer@msn.com>
2020-02-12 15:31:38 +0100

    graph added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,11 +11,12 @@ import { AppComponent } from './app.component';
 import { DetailComponent } from './detail/detail.component';
 import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
 import { BackgroundMode } from '@ionic-native/background-mode/ngx';
+import { ChartsModule } from 'ng2-charts'
 
 @NgModule({
   declarations: [AppComponent, DetailComponent],
   entryComponents: [DetailComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ChartsModule],
   providers: [
     StatusBar,
     SplashScreen,
